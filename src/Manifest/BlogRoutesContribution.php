df0cd7956dde078861937daa8256022ba0d0a30d
Ben <howdu@users.noreply.github.com>
2026-07-13 18:23:24 +0100

    fix: align extension manifests with v1 API

--- src/Manifest/BlogRoutesContribution.php
+++ src/Manifest/BlogRoutesContribution.php
@@ -11,6 +11,6 @@ final class BlogRoutesContribution implements ExtensionContribution, RegistersEx
 {
     public static function compatibleCapellApiVersion(): string
     {
-        return '^4.0';
+        return '^1.0';
     }
 }
